f8c4e31f7e5357d80d0a886efc0e8c9bc99bca1a
dilshad <dilshaat@gmail.com>
2018-03-25 22:07:06 +1100

    data conversion function added

--- tdm.py
+++ tdm.py
@@ -9,9 +9,32 @@ tdm_template = 'tdm.xlsx' # Empty Excel Template for TDMs
 wb = load_workbook(tdm_template) # Loding the Excel Template to memory
 files = os.listdir('.') # Listing all files in this current directory
 
+# TDM data type converter: Converts obscure SAP name to understandable english data type names
+# No Python or SAP database specific technical data type conversion is performed here
+def convert_data_type_names(old_data_type):
+    if old_data_type == 'LANG':
+        return 'LANGUAGE'
+    elif old_data_type == 'DATS':
+        return 'DATE'
+    elif old_data_type == 'NUMC':
+        return 'NUMERIC TEXT'
+    elif old_data_type == 'TIMS':
+        return 'Time HHMMSS'
+    elif old_data_type == 'LCHR':
+        return 'LONG CHAR'
+    elif old_data_type == 'RAW':
+        return 'Byte Sequence'
+    elif old_data_type == 'CLNT':
+        return 'Client'
+    elif old_data_type == 'DEC':
+        return 'Calculation/Amount Field'
+    else:
+        return old_data_type
+
+
 # Validating passed command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('objectname', help='The SAP object name')
+parser.add_argument('objectname', help='The SAP object name', choices=['proj', 'wbs', 'cost', 'compunit'])
 parser.add_argument('csv_file', help='SAP object spacific csv file to parse in the same folder')
 args = parser.parse_args()
 
@@ -47,11 +70,21 @@ wb_lt = wb[sheet_names[4]]
 # Setting Master Worksheet as focused worksheet
 wb.active = 0
 
+# Excel Sheet row counter
+ws_row_counter = 3
 
+# Reading CSV file and Writing to final TDM file
+with open(args.csv_file, 'r', newline='') as csv_file:
+	csv_lines = csv.reader(csv_file, delimiter=",")
+	for line in csv_lines:
+		#print(line[0], line[1], line[2], line[3], line[4], line[5], line[6].lstrip('0'))
+		ws_master.cell(row=ws_row_counter, column=2, value=line[0])
+		ws_master.cell(row=ws_row_counter, column=3, value=line[1])
+		ws_master.cell(row=ws_row_counter, column=4, value=line[2])
+		ws_master.cell(row=ws_row_counter, column=5, value=line[3])
+		ws_master.cell(row=ws_row_counter, column=6, value=line[4])
+		ws_master.cell(row=ws_row_counter, column=7, value=convert_data_type_names(line[5]))
+		ws_master.cell(row=ws_row_counter, column=8, value=line[6].lstrip('0'))
+		ws_row_counter += 1
 
 wb.save('tdm_test.xlsx')
-
-# with open(csv_files[0], 'r', newline='') as csv_file:
-# 	csv_lines = csv.reader(csv_file, delimiter="\t")
-# 	for line in csv_lines:
-# 		print(line)
